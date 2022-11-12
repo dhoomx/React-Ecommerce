b3451beede43336ecce19d5a3ca138d3ffbc3c56
dhoomx <61233196+dhoomx@users.noreply.github.com>
2022-11-13 02:50:06 +0800

    done

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import { mobile } from "../responsive";
+
+const Conttainer = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Conttainer>
+      <Navbar />
+      <Announcement />
+      <Title>Perfumes</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Products Filter</FilterText>
+          <Select>
+            <Option>Color</Option>
+            <Option>white</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Green</Option>
+            <Option>Pink</Option>
+          </Select>
+
+          <Select>
+            <Option>Size</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+            <Option>XXL</Option>
+            <Option>XXXL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Prodcuts</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Conttainer>
+  );
+};
+
+export default ProductList;
